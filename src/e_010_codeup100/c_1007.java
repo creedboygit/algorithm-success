bbce41df1ac9d42f882727f4efbb32bd381536d0
주현(리모델링개발팀) <rich@hanssem.com>
2023-01-10 16:00:24 +0900

    up

--- src/e_010_codeup100/c_1007.java
+++ src/e_010_codeup100/c_1007.java
@@ -1,10 +1,11 @@
 package e_010_codeup100;
 
+// 선문자 유니코드
+
 public class c_1007 {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("\u250C\u252C\u2510");
-        System.out.println("\u251C\u253C\u2524");
-        System.out.println("\u2514\u2534\u2518");
+
+        System.out.println("\"C:\\Download\\hello.cpp\"");
     }
 }
---
+++ src/e_010_codeup100/c_1011.java
@@ -0,0 +1,18 @@
+package e_010_codeup100;
+
+import java.util.Scanner;
+
+public class c_1011 {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
+//        char c = sc.next().charAt(0);
+//        System.out.println(c);
+
+        String s = sc.nextLine();
+        System.out.println(s);
+
+        sc.close();
+    }
+}
