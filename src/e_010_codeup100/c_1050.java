da67b330ce8ee79118c64f6ebe7a851b139408c0
주현(리모델링개발팀) <rich@hanssem.com>
2023-01-11 17:42:07 +0900

    up

---
+++ src/e_010_codeup100/c_1050.java
@@ -0,0 +1,20 @@
+package e_010_codeup100;
+
+import java.util.Scanner;
+
+//9 1
+
+public class c_1050 {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+
+        System.out.println(n == m ? 1 : 0);
+
+        sc.close();
+    }
+}
