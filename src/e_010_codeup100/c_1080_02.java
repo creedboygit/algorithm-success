43094cf91a0ea14999dd505f4408a08e645671e4
주현(리모델링개발팀) <rich@hanssem.com>
2023-01-12 15:44:59 +0900

    up

--- src/e_010_codeup100/c_1080_02.java
+++ src/e_010_codeup100/c_1080_02.java
@@ -3,21 +3,27 @@ package e_010_codeup100;
 import java.util.Scanner;
 
 /*
-2 3
+55
  */
 
-public class c_1081 {
+public class c_1080_02 {
 
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int m = sc.nextInt();
+        int sum = 0;
 
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= m; j++)
-                System.out.println(i + " " + j);
+        for (int i = 1; i <= n; i++) {
+
+            sum += i;
+
+            if (sum >= n) {
+                System.out.println(i);
+                break;
+            }
+        }
 
         sc.close();
     }
