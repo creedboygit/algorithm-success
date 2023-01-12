8694ef93d53d0fba8d5f9abd791d88ba0f9632ef
주현(리모델링개발팀) <rich@hanssem.com>
2023-01-12 17:23:18 +0900

    up

---
+++ src/e_010_codeup100/c_1085_02.java
@@ -0,0 +1,28 @@
+package e_010_codeup100;
+
+import java.util.Scanner;
+
+/*
+44100 16 2 10
+ */
+
+public class c_1085_02 {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        double h = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+        double s = sc.nextDouble();
+
+        double answer = 1;
+        answer = h * b * c * s;
+        answer /= 8;
+        answer /= 1024;
+        answer /= 1024;
+
+        System.out.printf("%.1f MB", answer);
+    }
+}
