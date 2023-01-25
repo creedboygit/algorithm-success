5bcda7a5f6eea2d685fc7d4314ce208bfd0f6af9
주현(리모델링개발팀) <rich@hanssem.com>
2023-01-25 17:29:26 +0900

    up

--- src/e_031_프로그래머스_고득점킷/g_016_탐욕법그리디/체육복_003_그리디_구현만.java
+++ src/e_031_프로그래머스_고득점킷/g_016_탐욕법그리디/체육복_003_그리디_구현만.java
@@ -33,7 +33,9 @@ public class 체육복_003_그리디_구현만 {
     public static int solution(int n, int[] lostArr, int[] reserveArr) {
 
         // 1. 정답 초기화
-        int answer = n - lostArr.length;
+//        int answer = n - lostArr.length;
+//        int answer = 0;
+        int count = 0;
 
         // 2. 배열 정렬
         Arrays.sort(lostArr);
@@ -45,7 +47,7 @@ public class 체육복_003_그리디_구현만 {
             loop: for (int j = 0; j < reserveArr.length; j++) {
                 // 자기 자신이 포함되어 있는지 체크
                 if (lostArr[i] == reserveArr[j]) {
-                    answer++; // 자신의 옷을 입으면 되니까 참여자 수 증가
+                    count++; // 자신의 옷을 입으면 되니까 참여자 수 증가
                     lostArr[i] = -1;
                     reserveArr[j] = -1;
                     break loop;
@@ -57,15 +59,19 @@ public class 체육복_003_그리디_구현만 {
         for (int i = 0; i < lostArr.length; i++) {
             loop: for (int j = 0; j < reserveArr.length; j++) {
                 // 잃어버린 사람거보다 1 작거나 큰거 가능하니까
-                if (lostArr[i] - 1 == reserveArr[j] || lostArr[i] + 1 == reserveArr[j]) {
-                    answer++;
+//                if (lostArr[i] - 1 == reserveArr[j] || lostArr[i] + 1 == reserveArr[j]) {
+                if (lostArr[i] == reserveArr[j] - 1 || lostArr[i] == reserveArr[j] + 1) {
+//                    answer++;
+                    count++;
                     reserveArr[j] = -1;
                     break loop;
                 }
             }
         }
 
-        return answer;
+        //결과값 : 전체 학생수 - 잃어버린 학생 수 + 빌린 학생 수
+        return n - lostArr.length + count;
+//        return answer;
     }
 
     public static void main(String[] args) throws Exception {
