d3f7faa6b1ff3c23fe41021b535a77ca6d3bcba7
주현(리모델링개발팀) <rich@hanssem.com>
2023-01-25 15:48:33 +0900

    up

--- src/e_031_프로그래머스_고득점킷/g_016_탐욕법그리디/체육복_002_그리디_HashSet.java
+++ src/e_031_프로그래머스_고득점킷/g_016_탐욕법그리디/체육복_002_그리디_HashSet.java
@@ -25,42 +25,43 @@ https://school.programmers.co.kr/learn/courses/30/lessons/42862
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 
 //Solution
 public class 체육복_002_그리디_HashSet {
 
     public static int solution(int n, int[] lostArr, int[] reserveArr) {
 
-        // 1. student 배열 생성
-        int[] student = new int[n + 2]; // 앞뒤로 빌려줘야 하므로 여분으로 앞 0, 뒤 6을 위해 2개를 추가해줌 (소스를 깔끔하게 하기 위해)
         int answer = 0;
 
-        // 2. reserve / lost 정보 반영
-        for (int l : lostArr)
-            student[l]--;
+        // 1. Set을 만든다.
+        HashSet<Integer> resList = new HashSet<>();
+        HashSet<Integer> losList = new HashSet<>();
 
-        for (int r : reserveArr)
-            student[r]++;
+        for (int i : reserveArr) {
+            resList.add(i);
+        }
 
-        // 3. 여분을 기준으로 앞뒤를 확인하여 체육복을 빌려준다.
-        // -1 : 도난당한 학생 / 0 : 본인것만 가지고 있는 학생 / 1 : 여분을 가지고 있는 학생
-        for (int i = 1; i <= n; i++) {
-            if (student[i] == 1) { // i번째 학생이 여분의 체육복을 가지고 있다면
-                if (student[i - 1] == -1) { // 앞 학생이 도난당한 학생일 경우
-                    student[i]--;
-                    student[i - 1]++;
-                } else if (student[i + 1] == -1) { // 뒤 학생이 도난당한 학생일 경우
-                    student[i]--;
-                    student[i + 1]++;
-                }
+        for (int i : lostArr) {
+            if (resList.contains(i)) {
+                resList.remove(i);
+            } else {
+                losList.add(i);
             }
         }
 
-        // 4. 체육복을 가지고 있는 학생수를 구한다.
-        for (int i = 1; i <= n; i++) {
-            if (student[i] >= 0)
-                answer++;
+        // 2. 여분을 기준으로 앞뒤를 확인하여 체육복을 빌려준다.
+        // -1 : 도난당한 학생 / 0 : 본인것만 가지고 있는 학생 / 1 : 여분을 가지고 있는 학생
+        for (int i : resList) {
+            if (losList.contains(i - 1)) { // 앞 학생
+                losList.remove(i - 1);
+            } else if (losList.contains(i + 1)) { // 뒤 학생
+                losList.remove(i + 1);
+            }
         }
+
+        // 3. 최대한 나눠준 후에 lost에 남아있는 학생들은 체육복이 없는 학생들이다.
+        answer = n - losList.size();
 
         return answer;
     }
