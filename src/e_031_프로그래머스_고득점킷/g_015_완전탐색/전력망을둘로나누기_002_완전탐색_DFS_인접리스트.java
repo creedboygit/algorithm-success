bda026c51d817dccfcf30c0d104e0a1f2239dcf6
주현(리모델링개발팀) <rich@hanssem.com>
2023-01-31 22:26:52 +0900

    up

--- src/e_031_프로그래머스_고득점킷/g_015_완전탐색/전력망을둘로나누기_002_완전탐색_DFS_인접리스트.java
+++ src/e_031_프로그래머스_고득점킷/g_015_완전탐색/전력망을둘로나누기_002_완전탐색_DFS_인접리스트.java
@@ -37,18 +37,11 @@ public class 전력망을둘로나누기_002_완전탐색_DFS_인접리스트 {
 
     public static int solution(int n, int[][] wires) {
 
-        //        System.out.println(Arrays.deepToString(lines));
-
-        // wires : 간선 정보
-        // wires[k] : [v1, v2] <- v1과 v2가 서로 이어짐
-
         int answer = -1;
 
         list = new ArrayList[n + 1];
-
         min = Integer.MAX_VALUE / 2;
 
-        // 인접리스트 만들기 초기화
         // 먼저 문제에서 주어진 n과 wires를 활용해 인접리스트를 만들어줬다.
         for (int i = 1; i <= n; i++) {
             list[i] = new ArrayList<>();
@@ -63,18 +56,15 @@ public class 전력망을둘로나누기_002_완전탐색_DFS_인접리스트 {
             list[v2].add(v1);
         }
 
-//        System.out.println(list);
-
-        // 인접리스트를 만든 뒤 wires의 크기만큼 반복문을 돌아주고, 각 반복마다 해당되는 v1과 v2를 dfs에 파라미터로 넘겨줬다.
         for (int i = 0; i < wires.length; i++) {
 
             int v1 = wires[i][0];
             int v2 = wires[i][1];
 
             visited = new boolean[n + 1];
-
             visited[1] = true;
 
+            // 인접리스트를 만든 뒤 wires의 크기만큼 반복문을 돌아주고, 각 반복마다 해당되는 v1과 v2를 dfs에 파라미터로 넘겨줬다.
             dfs(1, v1, v2);
 
             // dfs가 끝나면 visited 배열에 true라고 저장된 송전탑(cnt개)끼리 연결되어있고, false로 저장된 송전탑 끼리 연결되어 있기 때문에 두 그룹의 차이는 |cnt-(n-cnt)|가 된다.
@@ -83,7 +73,7 @@ public class 전력망을둘로나누기_002_완전탐색_DFS_인접리스트 {
                 if (visited[j]) cnt++;
             }
 
-            min = Math.min(min, Math.abs(cnt - (n - cnt)));
+            min = Math.min(min, Math.abs(n - (2 * cnt)));
         }
 
         answer = min;
@@ -93,14 +83,12 @@ public class 전력망을둘로나누기_002_완전탐색_DFS_인접리스트 {
 
     static void dfs(int n, int v1, int v2) {
 
-
         for (int num : list[n]) {
 
-            // dfs에서 현재 송전탑과 인접리스트에 있는 송전탑이 v1, v2인 경우는 건너뛰고
-            if ((v1 == n && v2 == num) || (v2 == num && v1 == num)) continue;
+            if ((n == v1 && num == v2) || (n == v2 && num == v1)) continue; // dfs에서 현재 송전탑과 인접리스트에 있는 송전탑이 v1, v2인 경우는 건너뛰고
 
-            // 인접리스트에 있는 송전탑이 아직 방문하지 않은(연결되지 않은) 송전탑이라면 dfs를 계속 실행시킨다.
-            if (!visited[num]) {
+            if (!visited[num]) { // 인접리스트에 있는 송전탑이 아직 방문하지 않은(연결되지 않은) 송전탑이라면 dfs를 계속 실행시킨다.
+
                 visited[num] = true;
                 dfs(num, v1, v2);
             }
