b667826c1d62909b6e2036abc50da49b1ebc9182
주현(리모델링개발팀) <rich@hanssem.com>
2023-01-31 14:32:07 +0900

    up

---
+++ src/e_031_프로그래머스_고득점킷/g_015_완전탐색/피로도_001_완전탐색_DFS.java
@@ -0,0 +1,74 @@
+package e_031_프로그래머스_고득점킷.g_015_완전탐색;
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/87946
+
+80	[[80,20],[50,40],[30,10]]	3
+
+
+3
+80
+80 20
+50 40
+30 10
+
+정답 : 3
+
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+//Solution
+public class 피로도_001_완전탐색_DFS {
+
+    static int answer = 0;
+    static boolean[] visit;
+
+    public static int solution(int k, int[][] dungeons) {
+
+        answer = 0;
+
+        visit = new boolean[dungeons.length];
+
+        dfs(k, dungeons, 0);
+
+        return answer;
+
+//        System.out.println(Arrays.deepToString(dungeons));
+    }
+
+    public static void dfs(int k, int[][] dungeons, int depth) {
+
+        for (int i = 0; i < dungeons.length; i++) {
+
+            if (!visit[i] && dungeons[i][0] <= k) {
+                visit[i] = true;
+                dfs(k - dungeons[i][1], dungeons, depth + 1);
+                visit[i] = false; // 백트래킹
+            }
+        }
+
+        answer = Math.max(answer, depth);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+
+        int[][] dungeons = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            dungeons[i][0] = Integer.parseInt(st.nextToken());
+            dungeons[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(solution(k, dungeons));
+    }
+}
