894cf4a6f33b66d9d72f0b48259603a360a73a15
주현(리모델링개발팀) <rich@hanssem.com>
2023-02-01 16:53:47 +0900

    up

--- src/e_031_프로그래머스_고득점킷/g_020_그래프/Main.java
+++
@@ -1,4 +0,0 @@
-package e_031_프로그래머스_고득점킷.g_020_그래프;
-
-public class Main {
-}
---
+++ src/e_031_프로그래머스_고득점킷/g_020_그래프/가장먼노드_001_그래프.java
@@ -0,0 +1,46 @@
+package e_031_프로그래머스_고득점킷.g_020_그래프;
+
+/*
+https://school.programmers.co.kr/learn/courses/30/lessons/49189
+
+6	[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]	3
+
+
+
+6 7
+3 6
+4 3
+3 2
+1 3
+1 2
+2 4
+5 2
+
+정답 : 3
+
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+//Solution
+public class 가장먼노드_001_그래프 {
+
+    public static int solution(int[][] graph) {
+
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
